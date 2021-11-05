2a2110df7179f3d085faf759792d1bb40134d4ce
Benedikt Dornauer <2010738480@stud.fh-kufstein.ac.at>
2021-11-05 16:07:51 +0100

    Working on Skeleton with Dennis.

--- prototypes/prototype_skeleton/index.js
+++ prototypes/prototype_skeleton/index.js
@@ -1,10 +1,54 @@
+const data = {
+    "rightCircle": {
+        "title": "Development & Engineering",
+        "sections": [
+            {
+                "id": "section-1",
+                "title": "Advanced Software Engineering & Architecture",
+                "background": "yellow",
+            },
+            {
+                "id": "section-2",
+                "title": "Advanced Software Engineering & Architecture",
+                "background": "yellow",
+            },
+            {
+                "id": "section-3",
+                "title": "Advanced Software Engineering & Architecture",
+                "background": "yellow",
+            },
+
+        ]
+    },
+    "leftCircle": {
+        "title": "Development & Engineering",
+        "sections": [
+            {
+                "id": "section-5",
+                "title": "Advanced Software Engineering & Architecture",
+                "background": "yellow",
+            },
+            {
+                "id": "section-5",
+                "title": "Advanced Software Engineering & Architecture",
+                "background": "yellow",
+            },
+            {
+                "id": "section-7",
+                "title": "Advanced Software Engineering & Architecture",
+                "background": "yellow",
+            },
+        ]
+    }
+}
+
+
 document.getElementById("section-1").addEventListener("mouseenter", () => changeText("Gewechselt in Bereich sec-1"));
 document.getElementById("section-2").addEventListener("mouseenter", () => changeText("Gewechselt in Bereich sec-2"));
 document.getElementById("section-3").addEventListener("mouseenter", () => changeText("Gewechselt in Bereich sec-3"));
 document.getElementById("section-4").addEventListener("mouseenter", () => changeText("Gewechselt in Bereich sec-4"));
 document.getElementById("section-5").addEventListener("mouseenter", () => changeText("Gewechselt in Bereich sec-5"));
 document.getElementById("section-6").addEventListener("mouseenter", () => changeText("Gewechselt in Bereich sec-6"));
-document.getElementById("section-7").addEventListener("mouseenter", () => changeText("Gewechselt in Bereich sec-7"));
 
 function changeText(text) {
     document.getElementById("selectedText").innerText = text;
