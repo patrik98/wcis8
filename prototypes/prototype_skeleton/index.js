3137816770342004692aaae40cafffbb01263060
Benedikt Dornauer <2010738480@stud.fh-kufstein.ac.at>
2021-11-05 18:14:26 +0100

    Working on Skeleton with Patrik & Denis. Added animation.

--- prototypes/prototype_skeleton/index.js
+++ prototypes/prototype_skeleton/index.js
@@ -43,6 +43,45 @@ function changeText(text) {
     document.getElementById("selectedText").innerText = text;
 }
 
+/* opacity */
+document.getElementsByClassName("gray-rings")[0].style.opacity = "0";
+document.getElementsByClassName("gray-rings")[1].style.opacity = "0";
+document.getElementById("section-1").style.opacity = "0";
+document.getElementById("section-2").style.opacity = "0";
+document.getElementById("section-3").style.opacity = "0";
+document.getElementById("section-4").style.opacity = "0";
+document.getElementById("section-5").style.opacity = "0";
+document.getElementById("section-6").style.opacity = "0";
+document.getElementById("section-center").style.opacity = "0";
+
+/* animation */
+document.getElementById("section-1").style.animation = "hideshow 1s forwards";
+document.getElementById("section-1").style.animationDelay = "0.5s";
+
+document.getElementById("section-2").style.animation = "hideshow 1s forwards";
+document.getElementById("section-2").style.animationDelay = "1s";
+
+document.getElementById("section-3").style.animation = "hideshow 1s forwards";
+document.getElementById("section-3").style.animationDelay = "1.5s";
+
+document.getElementById("section-center").style.animation = "hideshow 1s forwards";
+document.getElementById("section-center").style.animationDelay = "2s";
+
+document.getElementById("section-4").style.animation = "hideshow 1s forwards";
+document.getElementById("section-4").style.animationDelay = "2.5s";
+
+document.getElementById("section-5").style.animation = "hideshow 1s forwards";
+document.getElementById("section-5").style.animationDelay = "3s";
+
+document.getElementById("section-6").style.animation = "hideshow 1s forwards";
+document.getElementById("section-6").style.animationDelay = "3.5s";
+
+document.getElementsByClassName("gray-rings")[0].style.animation = "hideshow_rings 1s forwards";
+document.getElementsByClassName("gray-rings")[0].style.animationDelay = "4s";
+
+document.getElementsByClassName("gray-rings")[1].style.animation = "hideshow_rings 1s forwards";
+document.getElementsByClassName("gray-rings")[1].style.animationDelay = "4s";
+
 /*colors*/
 document.getElementById("section-1").style.fill = wcisContent.rightCircle["section-1"].backgroundColor;
 document.getElementById("section-2").style.fill = wcisContent.rightCircle["section-2"].backgroundColor;
