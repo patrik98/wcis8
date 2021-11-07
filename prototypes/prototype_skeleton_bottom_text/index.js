69987ca73622c6a9069fb0dfd62c25853beeb587
Benedikt Dornauer <2010738480@stud.fh-kufstein.ac.at>
2021-11-07 17:30:57 +0100

    prototpye for text under wcis8 fading in

---
+++ prototypes/prototype_skeleton_bottom_text/index.js
@@ -0,0 +1,98 @@
+const wcisContent = {
+    "rightCircle": {
+        "title": "Development & Engineering",
+    },
+    "leftCircle": {
+        "title": "Development & Engineering",
+    },
+    "sections": [
+        {
+            "id": 1,
+            "title": "Advanced Software Engineering & Architecture 1",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "backgroundColor": "#49853c",
+        },
+        {
+            "id": 2,
+            "title": "Advanced Software Engineering & Architecture 2",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "backgroundColor": "#549bd4"
+        },
+        {
+            "id": 3,
+            "title": "Advanced Software Engineering & Architecture 3",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "backgroundColor": "#9babb2"
+        },
+        {
+            "id": 4,
+            "title": "Advanced Software Engineering & Architecture 4",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "backgroundColor": "#3e73b7"
+        },
+        {
+            "id": 5,
+            "title": "Advanced Software Engineering & Architecture 5",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "backgroundColor": "#f1813b"
+        },
+        {
+            "id": 6,
+            "title": "Advanced Software Engineering & Architecture 6",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "backgroundColor": "#fabd22"
+        }
+    ]
+}
+
+
+/*initializiation sections*/
+let counter = 0.5;
+
+wcisContent.sections.forEach((section) => {
+    const sectionElement = document.getElementById("section-" + section.id);
+    sectionElement.addEventListener("mouseenter", () => changeText(section.title, section.description));
+    sectionElement.style.fill = section.backgroundColor;
+    sectionElement.style.opacity = "0";
+    sectionElement.style.animation = "hideshow 1s forwards";
+    sectionElement.style.animationDelay = counter + "s";
+    counter += 0.5;
+})
+
+function changeText(text, description) {
+    document.getElementById("sectionTitle").innerText = text;
+    document.getElementById("sectionDescription").innerText = description;
+    document.getElementById("informationBox").classList.add("fade-off");
+    setTimeout(function() {
+        document.getElementById("informationBox").classList.add("fade-on");
+        }, 400);
+    document.getElementById("informationBox").classList.remove("fade-on");
+
+}
+
+/*Animation text*/
+
+
+/* extra opacity */
+document.getElementsByClassName("gray-rings")[0].style.opacity = "0";
+document.getElementsByClassName("gray-rings")[1].style.opacity = "0";
+document.getElementById("section-center").style.opacity = "0";
+
+/* extra animation */
+document.getElementById("section-center").style.animation = "hideshow 1s forwards";
+document.getElementById("section-center").style.animationDelay = "2s";
+document.getElementsByClassName("gray-rings")[0].style.animation = "hideshow_rings 1s forwards";
+document.getElementsByClassName("gray-rings")[0].style.animationDelay = "4s";
+document.getElementsByClassName("gray-rings")[1].style.animation = "hideshow_rings 1s forwards";
+document.getElementsByClassName("gray-rings")[1].style.animationDelay = "4s";
+
+/*turn 8 - responsive view*/
+window.addEventListener("resize", function () {
+    if (window.screen.width >= 500) {
+        document.getElementById("wcis8").style.transform = "rotate(0deg)";
+        document.getElementById("wcis8").style.margin = "70px";
+
+    } else {
+        document.getElementById("wcis8").style.transform = "rotate(90deg)";
+    }
+})
