143cc3a9db19e2242147ef721921c446b93e5910
Sebastian Waldmann <sebastian.waldmann@adviseit.de>
2021-12-17 12:16:45 +0100

    feat: update content structure and adapt detail view for new structure

--- prototypes/prototype_react_loading_zoom/src/components/wcis8/detailView/DetailView.js
+++ prototypes/prototype_react_loading_zoom/src/components/wcis8/detailView/DetailView.js
@@ -11,20 +11,53 @@ import Section7 from './mini8Assets/WCIS8_section7.svg';
 
 
 function DetailView({detailViewKey, onResetZoom, content}) {
-    const [contentObject, setContentObject] = useState({
-        title: '',
-        text: '',
-        type: ''
-    })
+    const [contentObject, setContentObject] = useState([
+        {
+            "type": "title",
+            "text": "Hello World"
+        },
+        {
+            "type": "text",
+            "text": "Lorem Ipsum ..."
+        },
+        {
+            "type": "image",
+            "url": "https://picsum.photos/200/300",
+            "altText": ""
+        },
+        {
+            "type": "video",
+            "url": "https://www.youtube.com/embed/GXjyX-El1mU"
+        },
+        {
+            "type": "wordCloud",
+            "wordList": [
+                {
+                    "text": "abc",
+                    "settings": {
+                        "minFontSize": 12,
+                        "maxFontSize": 12
+                    }
+                },
+                {
+                    "text": "xyz",
+                    "settings": {
+                        "minFontSize": 17,
+                        "maxFontSize": 17
+                    }
+                }
+            ]
+        }
+    ])
 
     useEffect(() => {
         const detailContainer = document.querySelector('.detail-container');
         if (detailViewKey && detailViewKey.length > 0) {
-            detailContainer.classList.add('visible');
-
-            if (content && content.hasOwnProperty(detailViewKey)) {
-                setContentObject(content[detailViewKey])
+            if (content && content.hasOwnProperty('sections') && content.sections && content.sections.hasOwnProperty(detailViewKey) && content.sections[detailViewKey].hasOwnProperty(content) && content.sections[detailViewKey].content) {
+                setContentObject(content[detailViewKey].content)
             }
+
+            detailContainer.classList.add('visible');
         } else {
             detailContainer.classList.remove('visible');
         }
@@ -36,13 +69,44 @@ function DetailView({detailViewKey, onResetZoom, content}) {
 
     function wcis8SelectedView(value) {
         switch (value) {
-            case "rightTop":  return(<img style={{width: "20%"}} src={Section1} alt="Section 1"/>);
-            case "rightRight":  return(<img style={{width: "20%"}} src={Section2} alt="Section 2"/>);
-            case "rightBottom":  return(<img style={{width: "20%"}} src={Section3} alt="Section 3"/>);
-            case "middle":  return(<img style={{width: "20%"}} src={Section4} alt="Section 4"/>);
-            case "leftTop":  return(<img style={{width: "20%"}} src={Section5} alt="Section 5"/>);
-            case "leftLeft":  return(<img style={{width: "20%"}} src={Section6} alt="Section 6"/>);
-            case "leftBottom":  return(<img style={{width: "20%"}} src={Section7} alt="Section 7ç"/>);
+            case "rightTop":
+                return (<img style={{width: "20%"}} src={Section1} alt="Section 1"/>);
+            case "rightRight":
+                return (<img style={{width: "20%"}} src={Section2} alt="Section 2"/>);
+            case "rightBottom":
+                return (<img style={{width: "20%"}} src={Section3} alt="Section 3"/>);
+            case "middle":
+                return (<img style={{width: "20%"}} src={Section4} alt="Section 4"/>);
+            case "leftTop":
+                return (<img style={{width: "20%"}} src={Section5} alt="Section 5"/>);
+            case "leftLeft":
+                return (<img style={{width: "20%"}} src={Section6} alt="Section 6"/>);
+            case "leftBottom":
+                return (<img style={{width: "20%"}} src={Section7} alt="Section 7ç"/>);
+        }
+    }
+
+    function renderContentObjects(contentObject) {
+        switch (contentObject.type) {
+            case "title":
+                return (<h2 className={'text-5xl mb-4'}>{contentObject.text}</h2>);
+            case "text":
+                return (<p>
+                    {contentObject.text}
+                </p>);
+            case "video":
+                return (<iframe width="560" height="315" src={contentObject.url}
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen />);
+            case "image":
+                return (<img src={contentObject.url} alt={contentObject.altText ? contentObject.altText : ''}/>);
+            case "wordCloud":
+                return (
+                    <Cloud
+                        contentArray={contentObject.wordList.map(item => item.text)}/>);
+            default:
+                return null;
         }
     }
 
@@ -52,18 +116,7 @@ function DetailView({detailViewKey, onResetZoom, content}) {
             <div className={'detail-container'}>
                 <div className={'detail-text'}>
                     {wcis8SelectedView(contentObject.title)}
-                    <h2 className={'text-5xl mb-4'}>{contentObject.title}</h2>
-                    {contentObject.type === 'text' && <p>
-                        {contentObject.text}
-                        Key: {detailViewKey}
-                    </p>}
-                    {contentObject.type === 'video' && <div>
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/GXjyX-El1mU"
-                                title="YouTube video player" frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen></iframe>
-                    </div>}
-                    <Cloud contentArray={["topic1", "topic2","topic3", "topic4","topic5", "topic6","topic7", "topic8"]}/>
+                    {contentObject.map(item => renderContentObjects(item))}
                     <a onClick={onBackButtonClick} type={'button'} className={'mt-4'}>
                         Zurück
                     </a>
