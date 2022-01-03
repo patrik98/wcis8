b09eb4afcd6d44ed86e888180b348b49e3b9203f
Benedikt Dornauer <pabupi31>
2022-01-03 21:07:51 +0100

    fix: added key to cloud and added example content -> can be seen in coding (Tab 1)

--- react-app/src/components/wcis8/detailView/Cloud.js
+++ react-app/src/components/wcis8/detailView/Cloud.js
@@ -5,33 +5,36 @@ import {TagCloud} from 'react-tagcloud'
 
 function Cloud({contentArray}) {
 
-    const colors = ["#000000"] //array containing different colors for words in cloud
+    const colors = ["#49853c","#549bd4","#9babb2","#3e73b7","#f1813b","#fabd22"] //array containing different colors for words in cloud
     const maxWeight = Math.max(...contentArray.map(item => item.weight));
     const weightSteps = 1 / maxWeight
 
     function createData(minSize, maxSize) {
         let data = [];
+
         contentArray.forEach(content => {
             data.push({
+                key: content.text,
                 value: <div>{content.text}</div>,
                 count: Math.floor(((maxSize - minSize) / weightSteps * content.weight) + minSize),
                 color: colors[Math.floor(Math.random() * colors.length)]
             })
         })
+
         return data;
     }
 
     return (
         <div style={{ textAlign:"center"}}>
             <TagCloud
-                minSize={10}
-                maxSize={90}
+                minSize={20}
+                maxSize={60}
                 style={{
                     width: '100%',
                     height: '100%',
                 }}
                 shuffle
-                tags={createData(20, 40)}
+                tags={createData(0, 10)}
                 onClick={tag => alert(tag.text)}
             />
         </div>
