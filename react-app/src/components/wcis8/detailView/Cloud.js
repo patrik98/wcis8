13dd91340cc4171eac1c5d7db8e9222a4bc4683b
Benedikt Dornauer <benedikt.dornauer@student.uibk.ac.at>
2022-01-22 20:34:25 +0100

    added content for VR/AR: word-cloud, text & image

--- react-app/src/components/wcis8/detailView/Cloud.js
+++ react-app/src/components/wcis8/detailView/Cloud.js
@@ -5,7 +5,7 @@ import {TagCloud} from 'react-tagcloud'
 
 function Cloud({contentArray}) {
 
-    const colors = ["#49853c","#549bd4","#9babb2","#3e73b7","#f1813b","#fabd22"] //array containing different colors for words in cloud
+    const colors = ["#000000","#549bd4","#9babb2","#3e73b7","#f1813b","#fabd22"] //array containing different colors for words in cloud
     const maxWeight = Math.max(...contentArray.map(item => item.weight));
     const weightSteps = 1 / maxWeight
 
@@ -27,15 +27,14 @@ function Cloud({contentArray}) {
     return (
         <div style={{ textAlign:"center"}}>
             <TagCloud
-                minSize={20}
-                maxSize={60}
+                minSize={10}
+                maxSize={30}
                 style={{
                     width: '100%',
                     height: '100%',
                 }}
                 shuffle
                 tags={createData(0, 10)}
-                onClick={tag => alert(tag.text)}
             />
         </div>
 
